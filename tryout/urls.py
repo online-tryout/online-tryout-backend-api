eea45845651db39a90709ee85b4874434c5c2095
indramhaarta <indramhrt@gmail.com>
2024-06-04 09:11:36 +0700

    fix: add get list tryout

--- tryout/urls.py
+++ tryout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/tryout/health', views.health_check, name='health_check'),
     path('api/tryout/new', views.create_tryout, name='create_tryout'),
+    path('api/tryout/data', views.get_tryout, name='get_tryout'),
 ]
--- tryout/views.py
+++ tryout/views.py
@@ -1,5 +1,6 @@
 import os
 import pika
+import requests
 from django.http import JsonResponse
 from urllib.parse import urlparse
 from django.views.decorators.csrf import csrf_exempt
@@ -19,6 +20,20 @@ def create_tryout(request):
             return JsonResponse({'message': 'failed to publish into rabbit mq'}, status=500)
     else:
         return JsonResponse({'message': 'only post requests are allowed for this endpoint'}, status=405)
+
+@csrf_exempt
+def get_tryout(request):
+    if request.method == 'GET':
+        server_url = os.getenv('SERVER_URL')
+        url = server_url + "/api/db/tryout"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json() 
+            return JsonResponse(data)
+        else:
+            return JsonResponse({'message': 'Failed to fetch data from /api/db/tryout'}, status=response.status_code)
+    else:
+        return JsonResponse({'message': 'only get requests are allowed for this endpoint'}, status=405)
 
 
 
